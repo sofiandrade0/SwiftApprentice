2c7a915c2d41d31d5ddeed4b8fef1ca7d558c4d2
Sofia Andrade <sandrade@lcs.on.ca>
2020-10-21 10:27:41 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Sofia Andrade on 21/10/20.
+//  Copyright © 2020 Sofia Andrade. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
